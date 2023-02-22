15214467059fee69be7749b1286ad1536bec4fef
Jean Pereira dos Reis <jeanpereira.jpdr@gmail.com>
2023-02-22 15:09:45 +0000

    UPDATE - Atualizando pagina ImageUploader

--- src/Components/ImageUploader/index.jsx
+++ src/Components/ImageUploader/index.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Result({ score }) {
-  return <p>A pontuação de semelhança é {score.toFixed(2)}%.</p>;
+function ImageUploader() {
+  const [image1, setImage1] = useState(null);
+  const [image2, setImage2] = useState(null);
+
+  const handleImage1Change = (e) => {
+    setImage1(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleImage2Change = (e) => {
+    setImage2(URL.createObjectURL(e.target.files[0]));
+  };
+
+  return (
+    <div>
+      <h2>Carregar imagens:</h2>
+      <div>
+        <label htmlFor="image1">Imagem 1:</label>
+        <input type="file" id="image1" onChange={handleImage1Change} />
+      </div>
+      <div>
+        <label htmlFor="image2">Imagem 2:</label>
+        <input type="file" id="image2" onChange={handleImage2Change} />
+      </div>
+    </div>
+  );
 }
 
-export default Result;
+export default ImageUploader;
